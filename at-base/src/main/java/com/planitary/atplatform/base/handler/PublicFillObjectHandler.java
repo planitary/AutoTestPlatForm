4843f6ad344441efb0020dc8b3b5bc02bdd01ddc
planitary <853652156@qq.com>
2024-03-26 23:17:32 +0800

    20240326 mod:优化公共字段填充的日期格式化

--- at-base/src/main/java/com/planitary/atplatform/base/handler/PublicFillObjectHandler.java
+++ at-base/src/main/java/com/planitary/atplatform/base/handler/PublicFillObjectHandler.java
@@ -6,6 +6,7 @@ import org.apache.ibatis.reflection.MetaObject;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 /**
  * 公共字段填充
@@ -14,17 +15,19 @@ import java.time.LocalDateTime;
 @Slf4j
 public class PublicFillObjectHandler implements MetaObjectHandler {
 
+    // 格式化日期
+    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
     @Override
     public void insertFill(MetaObject metaObject) {
         log.info("公共字段填充，事件:insert");
-        metaObject.setValue("createTime", LocalDateTime.now());
-        metaObject.setValue("updateTime", LocalDateTime.now());
+        metaObject.setValue("createTime", LocalDateTime.now().format(formatter));
+        metaObject.setValue("updateTime", LocalDateTime.now().format(formatter));
     }
 
     @Override
     public void updateFill(MetaObject metaObject) {
         log.info("公共字段填充，事件:update");
-        metaObject.setValue("updateTime", LocalDateTime.now());
+        metaObject.setValue("updateTime", LocalDateTime.now().format(formatter));
 
     }
 }
